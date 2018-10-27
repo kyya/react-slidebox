90b660e14d850fcc5ff90e028857cb436ce00a0e
Anapopo <favoursmoe@gmail.com>
2018-10-27 22:16:40 +0800

    feat(webpack): build demo pages

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-import SlideBox from './components/slide-box'
+import React from 'react'
+import { render } from 'react-dom'
 
-export default SlideBox
+import 'react-slidebox/dist/react-slidebox.css'
+import ReactSlideBox from 'react-slidebox'
+
+const props = {
+    duration: 2000,
+    banners: [
+        'https://i.loli.net/2018/10/26/5bd3109e4e7f9.jpg',
+        'https://i.loli.net/2018/10/26/5bd3109f390aa.jpg',
+        'https://i.loli.net/2018/10/26/5bd3109f43a00.jpg',
+        'https://i.loli.net/2018/10/26/5bd310a0c47c0.jpg',
+        'https://i.loli.net/2018/10/26/5bd310a16546d.jpg'
+    ],
+    thumbs: [
+        'https://i.loli.net/2018/10/26/5bd310f62e777.jpg',
+        'https://i.loli.net/2018/10/26/5bd310f64c03b.jpg',
+        'https://i.loli.net/2018/10/26/5bd310f6c653d.jpg',
+        'https://i.loli.net/2018/10/26/5bd310f78c4f8.jpg',
+        'https://i.loli.net/2018/10/26/5bd310f8a7753.jpg'
+    ]
+}
+const AppStyle = {
+    margin: '2em',
+    display: 'flex',
+    justifyContent: 'center'
+}
+const App = () => (
+    <div className="container" style={AppStyle}>
+        <ReactSlideBox {...props} />
+    </div>
+)
+
+render(<App />, document.getElementById('root'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,70 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
-const webpack = require('webpack')
 
 module.exports = {
-    mode: 'production', 
+    mode: 'production',
     entry: {
-        'slide-box': './src/index.js'
+        app: './src/index.js',
+        framework: ['react', 'react-dom', 'prop-types']
     },
-    externals: ['react', 'prop-types'],
     devtool: 'source-map',
     output: {
-        filename: '[name].min.js',
-        path: path.resolve(__dirname, "lib"),
-        library: 'SlideBox',
-        libraryTarget: 'umd',
-        libraryExport: 'default',
-        umdNamedDefine: true,
-        publicPath: '/'
+        filename: '[name].[chunkhash:16].js',
+        path: path.resolve(__dirname, 'dist')
     },
 
     module: {
         rules: [
-        { 
-            test: /\.js$/, 
-            exclude: /node_modules/, 
-            loader: 'babel-loader'
-        },
-        { 
-            test: /\.scss$/, 
-            use: [
-                MiniCssExtractPlugin.loader,
-                {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true,
-                        localIdentName: '[name]--[local]-[hash:base64:5]'
-                    }
-                },
-                'sass-loader'
-            ]
-        }
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
         ]
     },
     optimization: {
         minimizer: [
-            new UglifyJSPlugin({
-                sourceMap: true,
-                uglifyOptions: {
-                    compress: true,
-                    warnings: false,
-                    output: {
-                        comments: false
-                    }
-                }
-            }),
+            new UglifyJSPlugin(),
             new OptimizeCSSAssetsPlugin()
-        ]
+        ],
+        splitChunks: {
+            chunks: "all",
+            minChunks: 1,
+            cacheGroups: {
+                framework: {
+                    priority: 200,
+                    test: "framework",
+                    name: "framework",
+                    enforce: true,
+                    reuseExistingChunk: true
+                }
+            }
+        }
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: '[name].min.css'
+            filename: '[name].[chunkhash:16].css'
         }),
-        new webpack.LoaderOptionsPlugin({
-            debug: false
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            inject: 'body',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true
+            },
         }),
-        new CleanWebpackPlugin(['lib'])
     ]
 }
 
